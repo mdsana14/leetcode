14d675b07b9875b8194142a3225012de4470fdd3
Md Sana <mailmdsana@gmail.com>
2026-03-14 16:10:42 +0530

    Time: 2 ms (98.69%) | Memory: 44.1 MB (47.80%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,10 +1,22 @@
 import java.math.BigInteger;
 class Solution {
     public String addStrings(String num1, String num2) {
-        BigInteger x = new BigInteger(num1);
-        BigInteger y = new BigInteger(num2);
-        BigInteger sum = x.add(y);
-        String r = String.valueOf(sum);
-        return r;
+        int i=num1.length()-1,j =num2.length()-1;
+        int c= 0;
+        StringBuilder ans = new StringBuilder();
+        while(i >= 0 || j >= 0 ){
+              int a = (i >= 0)?num1.charAt(i) -'0' : 0;
+              int b = (j >= 0)?num2.charAt(j) -'0' : 0;
+              int sum = a + b + c;
+              int d = sum % 10;
+              c = sum /10;
+              ans.append(d);
+              i--;
+              j--;
+        }
+        if(c > 0){
+            ans.append(1);
+        }
+        return ans.reverse().toString();
     }
 }
