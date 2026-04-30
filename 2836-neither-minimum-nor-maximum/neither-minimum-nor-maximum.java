d1f488f665a52ca95de9a7d4cd4d28486687c572
Md Sana <mailmdsana@gmail.com>
2026-04-30 16:03:55 +0530

    Time: 4 ms (100.00%) | Memory: 47 MB (63.23%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
@@ -1,8 +1,22 @@
 class Solution {
     public int findNonMinOrMax(int[] nums) {
         if(nums.length <= 2)return -1;
-        Arrays.sort(nums);
-        return nums[1];
-
+        int max = nums[0];
+        int min = nums[0];
+        int r = -1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] > max){
+                r = max;
+                max = nums[i];
+            }
+            else if(nums[i] < min){
+                r = min;
+                min = nums[i];
+            }
+            else{
+                r = nums[i];
+            }
+        }
+        return r;
     }
 }
