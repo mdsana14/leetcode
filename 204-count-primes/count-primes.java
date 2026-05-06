8b7fc9aec1acf8476ca4884a9cc85c90447ab581
Md Sana <mailmdsana@gmail.com>
2026-05-06 20:46:27 +0530

    Time: 98 ms (69.72%) | Memory: 51.2 MB (53.42%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n < 2)return 0;
+        boolean[] b = new boolean[n+1];
+        Arrays.fill(b,true);
+        b[0] = b[1] =false;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(b[i]){
+                for(int j=i*i;j<b.length;j += i){
+                    b[j] = false;
+                }
+            }
+        }
+        int c = 0;
+        for(int i=0;i<n;i++){
+            if(b[i]){
+                c++;
+            }
+        }
+        return c;
+    }
+}
