a2c1dbc063f7863b5de837761fcc0fe1a7c87a46
Md Sana <mailmdsana@gmail.com>
2026-07-17 11:19:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (64.88%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -13,13 +13,10 @@ class Solution {
          l.add(s.toString());
          return;
        }
-       System.out.println(s.toString());
        for(char c : op[digits.charAt(i) - '0'].toCharArray()){
          s.append(c);
-         System.out.println(s.toString());
          letter(digits,i + 1);
          s.setLength(s.length() - 1);
-         System.out.println(s.toString());
        }
     }
 }
